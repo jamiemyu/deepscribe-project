2ae75e0ea88eaa93d2dc550f980d139bbbeea87c
Jamie <jamieminyu@gmail.com>
2025-09-30 16:56:24 -0700

    Fixes eslint config imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
-import config from "eslint-config-deepscribe";
-import { defineConfig } from "@eslint/config-helpers";
+// @ts-check
 
-export default defineConfig([
-  config,
-]);
+import eslint from '@eslint/js';
+import { defineConfig, globalIgnores } from "@eslint/config-helpers";
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    [
+        globalIgnores([".next/*", "next-env.d.ts"]),
+        {
+            rules: {
+                semi: "error",
+                "prefer-const": "error",
+            },
+        },
+    ]
+);
